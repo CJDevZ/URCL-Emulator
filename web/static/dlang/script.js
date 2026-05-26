66b9b3c9ce351bbc4a118c3d932785fbc73230f0
CJDev <colin.goehler@gmail.com>
2026-05-27 00:36:34 +0200

    Made first steps for dlang

---
+++ web/static/dlang/script.js
@@ -0,0 +1,81 @@
+const fileLoader = document.getElementById('fileLoader');
+let editor = ace.edit("editor");
+
+editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true,
+    enableSnippets: false
+});
+editor.setTheme("ace/theme/twilight")
+editor.session.on('change', saveToLocal);
+
+let consoleOutput = document.getElementById('console-output');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedCode = localStorage.getItem('code');
+    if (savedCode) {
+        editor.setValue(savedCode, -1);
+    }
+});
+
+async function uploadCode() {
+    const code = editor.getValue();
+
+    try {
+        const response = await fetch(`urcl/compile`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(code)
+        });
+
+        if (!response.ok) {
+            consoleOutput.textContent = "Failed to Compile";
+            response.text().then(value => {
+                editor.session.setAnnotations(JSON.parse(value))
+                //editor.session.setAnnotations([{"row":2,"text":"Test Error","type":"error"}]);
+            });
+            return;
+        }
+
+        response.text().then(value => consoleOutput.textContent = value);
+        editor.session.setAnnotations([])
+    } catch (error) {
+        consoleOutput.textContent = "Upload failed: " + error.message;
+    }
+}
+
+function loadCode() {
+    fileLoader.click();
+}
+
+fileLoader.addEventListener('change', function() {
+    const file = this.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        const content = e.target.result;
+
+        editor.setValue(content, -1);
+        saveToLocal();
+    }
+
+    reader.readAsText(file);
+});
+
+function saveCode() {
+    const text = editor.getValue();
+    const blob = new Blob([text], { type: 'text/plain' });
+    const anchor = document.createElement('a');
+    anchor.download = 'program.urcl';
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.click();
+}
+
+function saveToLocal() {
+    const code = editor.getValue();
+    localStorage.setItem('code', code);
+}
